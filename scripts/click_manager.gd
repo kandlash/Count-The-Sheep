7a6b1bfc8b3e4290484b2b89683a3bbbeff9ccb0
konstantinlaskulov <konstantin.laschkulov@yandex.com>
2026-04-26 13:03:37 +0300

    sheep click fixed

--- scripts/click_manager.gd
+++ scripts/click_manager.gd
@@ -5,7 +5,7 @@ func _input(event):
 	and event.pressed \
 	and event.button_index == MOUSE_BUTTON_LEFT:
 		
-		_handle_click(event.position)
+		_handle_click(get_global_mouse_position())
 
 
 func _handle_click(pos: Vector2):
@@ -16,15 +16,13 @@ func _handle_click(pos: Vector2):
 	query.collide_with_areas = true
 	query.collide_with_bodies = false
 
-	var result = space.intersect_point(query, 32)
+	var result = space.intersect_point(query, 16)
 	if result.is_empty():
 		return
 
-	# берем ПЕРВУЮ подходящую овцу
 	for hit in result:
 		var obj = hit.collider
 		if obj.is_in_group("sheep"):
-			print('sheep click - ', obj.get_parent())
 			AudioManager.create_audio(SoundEffect.SOUND_EFFECT_TYPE.SHEEP_CLICK)
 			obj.get_parent().click_animation()
 			obj.get_parent().request_jump()
